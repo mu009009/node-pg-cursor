dd62195a6ff18fa63ada8defadaaab5b5e607640
Xiaxin Chen <xiaxinchen@c435a100g3qnaws.local>
2017-01-30 01:59:36 -0500

    delete all pg.js

--- pg.js
+++ pg.js
@@ -3,11 +3,7 @@ var pgPath;
 //support both pg & pg.js
 //this will eventually go away when i break native bindings
 //out into their own module
-try {
-  pgPath = path.dirname(require.resolve('pg'))
-} catch(e) {
-  pgPath = path.dirname(require.resolve('pg.js')) + '/lib'
-}
+pgPath = path.dirname(require.resolve('pg'))
 
 module.exports.Result = require(path.join(pgPath, 'result.js'))
 module.exports.prepareValue = require(path.join(pgPath, 'utils.js')).prepareValue
