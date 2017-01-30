6a1e3074f221c3419fe72333dc5a7eed01896cce
Xiaxin Chen <xiaxinchen@c435a100g3qnaws.local>
2017-01-30 12:55:52 -0500

    omit path to avoid some bugs

--- pg.js
+++ pg.js
@@ -1,9 +1,11 @@
-var path = require('path')
-var pgPath;
-//support both pg & pg.js
-//this will eventually go away when i break native bindings
-//out into their own module
-pgPath = path.dirname(require.resolve('pg'))
-
-module.exports.Result = require(path.join(pgPath, 'result.js'))
-module.exports.prepareValue = require(path.join(pgPath, 'utils.js')).prepareValue
+// var path = require('path')
+// var pgPath;
+// //support both pg & pg.js
+// //this will eventually go away when i break native bindings
+// //out into their own module
+// pgPath = path.dirname(require.resolve('pg'))
+var pg = require('pg');
+// module.exports.Result = require(path.join(pgPath, 'result.js'))
+// module.exports.prepareValue = require(path.join(pgPath, 'utils.js')).prepareValue
+module.exports.Result = require('pg/lib/result.js');
+module.exports.prepareValue = require('pg/lib/utils.js').prepareValue;
